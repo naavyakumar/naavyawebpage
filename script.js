9742bdb633a4a43842516895e6a56707320aa4a8
naavyakumar <145372525+naavyakumar@users.noreply.github.com>
2023-09-23 14:29:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -29,3 +29,151 @@ tl.to(".Loading", {
     },
     "up"
   )
+  .to(
+    ".svgwaala",
+    {
+      opacity: 1,
+      delay: -1.7,
+      ease: "Expo.easeInOut",
+    },
+    "donut"
+  )
+  .to(
+    "#dotted",
+    {
+      opacity: 1,
+      delay: -1.8,
+      ease: "Expo.easeInOut",
+    },
+    "donut"
+  )
+  .to(
+    "#donut",
+    {
+      opacity: 1,
+      scale: 2,
+      delay: -1.4,
+      ease: "Expo.easeInOut",
+      rotationY: "+=15",
+      duration: 3,
+      rotationX: "+=15",
+      rotationZ: "+=15",
+    },
+    "up"
+  )
+
+  .from(
+    "#kothik-k, #kothik-o, #kothik-t, #kothik-h, #kothik-k2, #kothik-i",
+    {
+      opacity: 0,
+      duration: 1,
+      scale: 0,
+      top: "40%",
+      left: "47%",
+    },
+    "up"
+  );
+
+var tl2 = gsap.timeline();
+
+document.querySelectorAll("#explore").forEach(function (elem) {
+  elem.addEventListener("click", function () {
+    tl2
+      .to(
+        "#NAAVYA K-N",
+        {
+          left: "-15%",
+          top: "-15%",
+          scale: "2",
+          rotate: "25deg",
+          ease: "ease.out",
+          duration: 1,
+          opacity: 0,
+        },
+        "sw"
+      )
+      .to(
+        "#NAAVYA K-A",
+        {
+          top: "-20%",
+          scale: "2",
+          rotate: "25deg",
+          ease: "ease.out",
+          duration: 1,
+          opacity: 0,
+        },
+        "sw"
+      )
+      .to(
+        "#NAAVYA-A2",
+        {
+          left: "115%",
+          top: "-20%",
+          scale: "2",
+          rotate: "25deg",
+          ease: "ease.out",
+          duration: 1,
+          opacity: 0,
+        },
+        "sw"
+      )
+      .to(
+        "#NAAVYA K-V",
+        {
+          left: "115%",
+          top: "110%",
+          scale: "2",
+          rotate: "25deg",
+          ease: "ease.out",
+          duration: 1,
+          opacity: 0,
+        },
+        "sw"
+      )
+      .to(
+        "#NAAVYA K-Y",
+        {
+          left: "35%",
+          top: "120%",
+          scale: "2",
+          rotate: "25deg",
+          ease: "ease.out",
+          duration: 1,
+          opacity: 0,
+        },
+        "sw"
+      )
+      .to(
+        "NAAVYA K-A3",
+        {
+          left: "-15%",
+          top: "80%",
+          scale: "2",
+          rotate: "25deg",
+          ease: "ease.out",
+          duration: 1,
+          opacity: 0,
+        },
+        "sw"
+      )
+      .to("#donut", {
+        width: "80vw",
+        top: "-10%",
+        rotate: "360deg",
+        ease: "ease.out",
+        duration: 1,
+        opacity: 0,
+        delay: "-1",
+      })
+      .to(
+        "#dotted",
+        {
+          opacity: 0,
+        },
+        "sw"
+      )
+      .to(".svgwaala", {
+        opacity: 0,
+      });
+  });
+});
